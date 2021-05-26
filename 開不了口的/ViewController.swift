74a7b6e5410385a762af25e4ae9144b5e2209f25
Andeline <andelineya@gmail.com>
2021-05-26 18:49:48 +0800

    Initial Commit

---
+++ 開不了口的/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  開不了口的
+//
+//  Created by 蔡佳穎 on 2021/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
